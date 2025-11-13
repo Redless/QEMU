6adab969368432cab69591af83b144c61768cf9d
Gabriel Brookman <brookmangabriel@gmail.com>
2025-11-13 18:30:06 -0500

    target/arm: add canonical and no-address tag logic
    This feature causes tag checks to compare logical address tags against
    their canonical form rather than against allocation tags. Described in
    the ARM ARM section "Logical Address Tagging".
    
    Signed-off-by: Gabriel Brookman <brookmangabriel@gmail.com>

--- target/arm/tcg/mte_helper.c
+++ target/arm/tcg/mte_helper.c
@@ -853,6 +853,13 @@ static int mte_probe_int(CPUARMState *env, uint32_t desc, uint64_t ptr,
         mem1 = allocation_tag_mem(env, mmu_idx, ptr, type, sizem1 + 1,
                                   MMU_DATA_LOAD, ra);
         if (!mem1) {
+            /*
+             * If mtx is enabled, then the access is MemTag_CanonicallyTagged,
+             * otherwise it is MemTag_AllocationTagged. See AArch64.CheckTag.
+             */
+            if (mtx_check(desc, bit55)) {
+                return tag_is_canonical(ptr_tag, bit55);
+            }
             return 1;
         }
         /* Perform all of the comparisons. */
@@ -865,6 +872,12 @@ static int mte_probe_int(CPUARMState *env, uint32_t desc, uint64_t ptr,
         mem2 = allocation_tag_mem(env, mmu_idx, next_page, type,
                                   ptr_last - next_page + 1,
                                   MMU_DATA_LOAD, ra);
+
+        /* If either region is canonically tagged, do a canonical tag check */
+        if (mtx_check(desc, bit55) && (!mem1 || !mem2)
+            && (!tag_is_canonical(ptr_tag, bit55))) {
+            return 0;
+        }
 
         /*
          * Perform all of the comparisons.
@@ -972,6 +985,18 @@ uint64_t HELPER(mte_check_zva)(CPUARMState *env, uint32_t desc, uint64_t ptr)
     if (tcma_check(desc, bit55, ptr_tag)) {
         goto done;
     }
+
+    /*
+     * If mtx is enabled, then the access is MemTag_CanonicallyTagged,
+     * otherwise it is MemTag_AllocationTagged. See AArch64.CheckTag.
+     */
+    if (mtx_check(desc, bit55)) {
+        if (tag_is_canonical(ptr_tag, bit55)) {
+            goto done;
+        }
+        mte_check_fail(env, desc, ptr, ra);
+    }
+
 
     /*
      * In arm_cpu_realizefn, we asserted that dcz > LOG2_TAG_GRANULE+1,
