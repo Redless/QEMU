7be20804a74e1c73f79962db2fa4a8d68e6ce645
Gabriel Brookman <brookmangabriel@gmail.com>
2025-11-13 18:30:06 -0500

    target/arm: add canonical and no-address tag logic
    This feature causes tag checks to compare logical address tags against
    their canonical form rather than against allocation tags. Described in
    the ARM ARM section "Logical Address Tagging".
    
    Signed-off-by: Gabriel Brookman <brookmangabriel@gmail.com>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -1545,7 +1545,8 @@ FIELD(MTEDESC, TBI,   4, 2)
 FIELD(MTEDESC, TCMA,  6, 2)
 FIELD(MTEDESC, WRITE, 8, 1)
 FIELD(MTEDESC, ALIGN, 9, 3)
-FIELD(MTEDESC, SIZEM1, 12, 32 - 12)  /* size - 1 */
+FIELD(MTEDESC, MTX,   12, 2)
+FIELD(MTEDESC, SIZEM1, 14, 32 - 14)  /* size - 1 */
 
 bool mte_probe(CPUARMState *env, uint32_t desc, uint64_t ptr);
 uint64_t mte_check(CPUARMState *env, uint32_t desc, uint64_t ptr, uintptr_t ra);
@@ -1619,6 +1620,12 @@ static inline uint64_t address_with_allocation_tag(uint64_t ptr, int rtag)
 static inline bool tbi_check(uint32_t desc, int bit55)
 {
     return (desc >> (R_MTEDESC_TBI_SHIFT + bit55)) & 1;
+}
+
+/* Return true if mtx bits mean that the access is canonically checked.  */
+static inline bool mtx_check(uint32_t desc, int bit55)
+{
+    return (desc >> (R_MTEDESC_MTX_SHIFT + bit55)) & 1;
 }
 
 /* Return true if tcma bits mean that the access is unchecked.  */
