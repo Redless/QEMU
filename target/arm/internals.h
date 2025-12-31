e9c68d2a59bc46566affaf3c37f8eda23a10c55f
Gabriel Brookman <brookmangabriel@gmail.com>
2025-12-31 15:22:54 -0800

    target/arm: added MTX bits to MTE check logic
    Previously, the TBI bit was used to mediate whether MTE checks happened.
    With MTE4, if the MTX bits are enabled, then tag checking happens even
    if TBI is disabled. See AccessIsTagChecked.
    
    Signed-off-by: Gabriel Brookman <brookmangabriel@gmail.com>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -1410,6 +1410,7 @@ ARMVAParameters aa64_va_parameters(CPUARMState *env, uint64_t va,
                                    ARMMMUIdx mmu_idx, bool data,
                                    bool el1_is_aa32);
 
+int aa64_va_parameter_mtx(uint64_t tcr, ARMMMUIdx mmu_idx);
 int aa64_va_parameter_tbi(uint64_t tcr, ARMMMUIdx mmu_idx);
 int aa64_va_parameter_tbid(uint64_t tcr, ARMMMUIdx mmu_idx);
 int aa64_va_parameter_tcma(uint64_t tcr, ARMMMUIdx mmu_idx);
